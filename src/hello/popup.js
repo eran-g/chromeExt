8a6f780dd9a223284028fce4d8027a56a4a4467d
erangw2v <erang@way2vat.com>
2018-02-24 02:15:13 +0200

    init

--- src/hello/popup.js
+++ src/hello/popup.js
@@ -1,6 +1,66 @@
+let icon
+let interval
+
+document.addEventListener('DOMContentLoaded', () => {
+
+        // init UI elements event listeners
+        const startBtn = document.getElementById('start')
+        const stopBtn = document.getElementById('stop')
+
+        startBtn.onclick = () => {
+            startBtn.disabled = true
+            stopBtn.disabled = false
+
+            interval = setInterval(() => {
+                icon = !icon || icon === 'green.png' ? 'red.png' : 'green.png'
+
+                chrome.browserAction.setIcon({
+                    path: icon
+                })
+            }, 1000)
+
+            console.log('--->interval:', interval)
+        }
+
+        stopBtn.onclick = () => {
+            stopBtn.disabled = true
+            startBtn.disabled = false
+
+            clearInterval(interval)
+        }
+
+})
+
+
+chrome.runtime.onMessage.addListener((message,sender,sendResponse) => {
+    switch (message.msg) {
+        case 'handShakeFromBg':
+            sendResponse({
+                msg: 'handShake resp From pop'
+            })
+            break
+        case 'requestFailed':
+            // change icon and alarm
+            // send response: clear interval
+            break
+    }
+})
+console.log('--->popJsListener listening')
+// test messages and response
+const handShakeresp = popJsSendMsg({
+    msg: 'handShake'
+})
+console.log('--->handShakeresp:', JSON.stringify(handShakeresp))
+
+const testMsgResp = popJsSendMsg({
+    msg: 'testMsg'
+})
+console.log('--->testMsgResp:', JSON.stringify(testMsgResp))
+
+
 async function popJsSendMsg(msg) {
     return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage(msg, function(response){
+        chrome.runtime.sendMessage(msg, (response) =>{
             if (response) {
                 resolve(response)
             }else {
@@ -9,35 +69,3 @@ async function popJsSendMsg(msg) {
         })
     })
 }
-
-async function popJsListener() {
-    return new Promise((resolve) => {
-        chrome.runtime.onMessage.addListener(function(message,sender,sendResponse) {
-            switch (message.msg) {
-                case 'handShakeFromBg':
-                    sendResponse({
-                        msg: 'handShake resp From pop'
-                    })
-                    break
-            }
-        })
-        console.log('--->popJsListener listening')
-        resolve()
-    })
-}
-
-async function f() {
-    await popJsListener()
-
-    const handShakeresp = await popJsSendMsg({
-        msg: 'handShake'
-    })
-    console.log('--->handShakeresp:', JSON.stringify(handShakeresp))
-
-    const testMsgResp = await popJsSendMsg({
-        msg: 'testMsg'
-    })
-    console.log('--->testMsgResp:', JSON.stringify(testMsgResp))
-}
-
-f()
